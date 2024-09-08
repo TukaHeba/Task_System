6d702af799669fd5fe2397f605cbfb7dc16233d5
Tuka Heba <tuka.heba@gmail.com>
2024-09-08 23:31:38 +0300

    Add Task Resource

---
+++ app/Http/Resources/TaskResource.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TaskResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'task_id' => $this->task_id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'priority' => $this->priority,
+            'due_date' => $this->due_date,
+            'status' => $this->status,
+            'assigned_to' => $this->user ? [
+                'id' => $this->user->id,
+                'name' => $this->user->name,
+                'role' => $this->user->role,
+            ] : null,
+            'created_by' => $this->creator ? [
+                'id' => $this->creator->id,
+                'name' => $this->creator->name,
+            ] : null,
+            'created_on' => $this->created_on,
+            'updated_on' => $this->updated_on,
+            'deleted_at' => $this->deleted_at,
+        ];
+    }
+}
